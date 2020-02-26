4cad6d99b9f8161429d714d82e3a7f7356734dac
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-26 14:14:41 -0700

    adding routes

--- server.js
+++ server.js
@@ -1,37 +1,26 @@
 const express = require("express");
-const morgan = require("morgan");
-const app = express();
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
 
-//middleware
-app.use(morgan("dev"));
-// parses req.body
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// serves up static assets
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.json({
-        food: "pork"
-    })
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
-mongoose
-    .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }) 
-    .then(() =>{
-        console.log("connected to db")
-    })
-    .catch( (err) => {
-        console.log(err)
-    })
 
-const PORT = process.env.PORT || 3000
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
-console.log(`connected to PORT: ${PORT}`)
+  console.log(`App running on port ${PORT}!`);
 });
